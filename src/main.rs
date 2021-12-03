14c2f6a713cb0554611a47c586b6f555edf46a8e
Avimitin <avimitin@gmail.com>
2021-12-03 21:50:33 +0800

    N: add command line option
    Enable user to use command like:
      * cli get-beatmap -b <BID> -s <SID>
      * cli get-user -u <USERNAME>
    
    Signed-off-by: Avimitin <avimitin@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
-use anyhow::Error;
+use anyhow::{bail, Error};
 use osu_query::{get_beatmaps, get_users, AppConfig};
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let opt = CommandLineOption::from_args();
+
     let cfg: AppConfig = confy::load("osu-query")?;
 
-    let resp = get_beatmaps(&cfg.api_key, "", "", "1872396").await?;
-    println!("{:?}", resp);
+    match opt {
+        CommandLineOption::GetBeatmap {
+            mode,
+            beatmap_set,
+            beatmap_id,
+        } => {
+            if beatmap_set == "" && beatmap_id == "" {
+                bail!("You need at lease give one argument about the song")
+            }
 
-    let resp = get_users(&cfg.api_key, "CookieBacon").await?;
-    println!("{:?}", resp);
+            let resp = get_beatmaps(&cfg.api_key, &mode, &beatmap_set, &beatmap_id).await?;
+            println!("{:?}", resp);
+        }
+        CommandLineOption::GetUser { user } => {
+            let resp = get_users(&cfg.api_key, &user).await?;
+            println!("{:?}", resp);
+        }
+    }
+
     Ok(())
+}
+
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(about = "A command line osu data fetcher")]
+enum CommandLineOption {
+    #[structopt(alias = "gb", about = "Get beatmap information")]
+    GetBeatmap {
+        #[structopt(short, long, default_value = "0", about = "Specify beatmap mode")]
+        mode: String,
+
+        #[structopt(short = "s", long, default_value = "")]
+        beatmap_set: String,
+
+        #[structopt(short = "b", long, default_value = "")]
+        beatmap_id: String,
+    },
+
+    #[structopt(alias = "gu", about = "Get user information")]
+    GetUser {
+        #[structopt(short = "u", long, default_value = "")]
+        user: String,
+    },
 }
